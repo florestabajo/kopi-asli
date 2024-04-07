21459b3ba9e6ced1001625578bbdd2633401992a
li1n02002 <165244083+li1n02002@users.noreply.github.com>
2024-04-07 22:49:22 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
-document.querySelectorAll('.menu-item').forEach(item => {
-    item.addEventListener('mouseenter', () => {
-      item.querySelector('img').style.transform = 'scale(1.1)';
-    });
-  
-    item.addEventListener('mouseleave', () => {
-      item.querySelector('img').style.transform = 'scale(1)';
-    });
-  });
-  // Update waktu setiap detik
-function updateClock() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-    document.getElementById('hours').textContent = hours;
-    document.getElementById('minutes').textContent = minutes;
-    document.getElementById('seconds').textContent = seconds;
-  }
-  
-  // Memanggil fungsi untuk pertama kali
-  updateClock();
-  
-  // Memanggil fungsi setiap detik
-  setInterval(updateClock, 1000);
-  
+function togglePlayPause() {
+    var video = document.getElementById("myVideo");
+    var button = document.getElementById("playPauseBtn");
+
+    if (video.paused || video.ended) {
+        video.play();
+        button.innerHTML = "Pause";
+    } else {
+        video.pause();
+        button.innerHTML = "Play";
+    }
+}
